9a6eda8263ee207586e10d7a5fbf09ae842b44bf
Agustin Garcia <a.garcia@pnptc.com>
2021-04-27 21:54:37 +0200

    Defaults update

--- src/models/favorites.ts
+++ src/models/favorites.ts
@@ -1,11 +1,140 @@
 export const starterSetOfFavorites = [
-  { name: 'Angular Red', value: '#dd0531' },
-  { name: 'Azure Blue', value: '#007fff' },
-  { name: 'JavaScript Yellow', value: '#f9e64f' },
-  { name: 'Mandalorian Blue', value: '#1857a4' },
-  { name: 'Node Green', value: '#215732' },
-  { name: 'React Blue', value: '#61dafb' },
-  { name: 'Something Different', value: '#832561' },
-  { name: 'Svelte Orange', value: '#ff3d00' },
-  { name: 'Vue Green', value: '#42b883' },
+  {
+    name: 'PKMN Red',
+    value: '#ff1111',
+  },
+  {
+    name: 'PKMN Blue',
+    value: '#1111ff',
+  },
+  {
+    name: 'PKMN Yellow',
+    value: '#ffd733',
+  },
+  {
+    name: 'PKMN Gold',
+    value: '#daa520',
+  },
+  {
+    name: 'PKMN Silver',
+    value: '#c0c0c0',
+  },
+  {
+    name: 'PKMN Crystal',
+    value: '#4fd9ff',
+  },
+  {
+    name: 'PKMN Ruby',
+    value: '#a00000',
+  },
+  {
+    name: 'PKMN Sapphire',
+    value: '#0000a0',
+  },
+  {
+    name: 'PKMN Emerald',
+    value: '#00a000'
+  },
+  {
+    name: 'PKMN FireRed',
+    value: '#ff7327',
+  },
+  {
+    name: 'PKMN LeafGreen',
+    value: '#00dd00',
+  },
+  {
+    name: 'PKMN Colloseum',
+    value: '#b6cae4',
+  },
+  {
+    name: 'PKMN XD: Gale of Darkness',
+    value: '#604e82',
+  },
+  {
+    name: 'PKMN Diamond',
+    value: '#aaaaff',
+  },
+  {
+    name: 'PKMN Pearl',
+    value: '#ffaaaa',
+  },
+  {
+    name: 'PKMN Platinum',
+    value: '#999999',
+  },
+  {
+    name: 'PKMN HeartGold',
+    value: '#b69e00',
+  },
+  {
+    name: 'PKMN SoulSilver',
+    value: '#c0c0e1',
+  },
+  {
+    name: 'PKMN Black',
+    value: '#444444',
+  },
+  {
+    name: 'PKMN White',
+    value: '#e1e1e1',
+  },
+  {
+    name: 'PKMN X',
+    value: '#6376b8',
+  },
+  {
+    name: 'PKMN Y',
+    value: '#ed5540',
+  },
+  {
+    name: 'PKMN Omega Ruby',
+    value: '#cf3025',
+  },
+  {
+    name: 'PKMN Alpha Sapphire',
+    value: '#1768d1',
+  },
+  {
+    name: 'PKMN Sun',
+    value: '#f1912b',
+  },
+  {
+    name: 'PKMN Moon',
+    value: '#5599ca',
+  },
+  {
+    name: 'PKMN Ultra Sun',
+    value: '#e35627',
+  },
+  {
+    name: 'PKMN Ultra Moon',
+    value: '#1f62a3',
+  },
+  {
+    name: 'PKMN Let\'s go Eevee',
+    value: '#a97a56'
+  },
+  {
+    name: 'PKMN Let\'s go Pikachu',
+    value: '#f6e652'
+  },
+  {
+    name: 'PKMN Sword',
+    value: '#19e5e6'
+  },
+  {
+    name: 'PKMN Shield',
+    value: '#e61747'
+  }
 ];
+//fff6a4 amarillo más pálido
+//c08c5c
+//a97a56
+
+// ho-oh
+// rojo c23316
+// verde 8a9b2d
+// blanco e5e1cb
+// naranja f59c00
+// amarillo fad876
